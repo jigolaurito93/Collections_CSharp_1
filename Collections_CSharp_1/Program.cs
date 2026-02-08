38a07328e3e422286d5f6a509b72a2f72254e7d8
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-08 05:37:06 -0500

    using lists and iterating with foreach

--- Collections_CSharp_1/Program.cs
+++ Collections_CSharp_1/Program.cs
@@ -10,6 +10,25 @@ namespace Collections_CSharp_1
     {
         static void Main(string[] args)
         {
+            /*
+            List<int> numbers = new List<int>(); // This is like saying "Here's a box for holding numbers."
+            numbers.Add(1); // Add the number 1 to the box.
+            numbers.Add(2); // Add the number 2 to the box.
+            */
+
+            // Creating a list of colors but only taking strings
+            List<string> colors = new List<string>();
+            // Method for Lists that adds a value
+            colors.Add("Black");
+            colors.Add("Yellow");
+            colors.Add("Green");
+            colors.Add("Blue");
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
         }
     }
 }
