095bf03c670c5e1693a44599d3e355f8ff420481
jigolaurito93 <jigolaurito93@yahoo.com>
2026-02-08 16:01:16 -0500

    configured for mac

---
+++ Collections_CSharp_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_CSharp_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            List<int> numbers = new List<int>(); // This is like saying "Here's a box for holding numbers."
+            numbers.Add(1); // Add the number 1 to the box.
+            numbers.Add(2); // Add the number 2 to the box.
+            */
+
+            // Creating a list of colors but only taking strings
+            List<string> colors = new List<string>();
+            // Method for Lists that adds a value
+            colors.Add("Black");
+            colors.Add("Yellow");
+            colors.Add("Green");
+            colors.Add("Blue");
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+        }
+    }
+}
