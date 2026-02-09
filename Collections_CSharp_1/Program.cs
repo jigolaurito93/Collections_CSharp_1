dbf0b28051a6fea33ed835fad53e202a34c3803a
jigolaurito93 <jigolaurito93@yahoo.com>
2026-02-09 06:16:18 -0600

    used remove and removeall methods for lists

--- Collections_CSharp_1/Program.cs
+++ Collections_CSharp_1/Program.cs
@@ -22,14 +22,40 @@ namespace Collections_CSharp_1
             colors.Add("Black");
             colors.Add("Yellow");
             colors.Add("Green");
-            colors.Add("Blue");
-            colors.Add("Brown");
+            colors.Add("Black");
 
-            foreach (string color in colors)
+            static void PrintColors(List<string> colors)
             {
-                Console.WriteLine(color);
+                Console.WriteLine("These are the colors in the list:");
+
+                foreach (string color in colors)
+                {
+                    Console.WriteLine("- " + color);
+                }
             }
 
-        }
+            PrintColors(colors);
+
+            // method to remove a value from the list
+            colors.Remove("Black"); // This will remove the first occurrence of "Black" from the list.
+
+            PrintColors(colors);
+
+            // Remove method also returns boolean value to indicate if the item was successfully removed or not
+            bool isColorRemoved = colors.Remove("Yellow");
+            Console.WriteLine($"Was 'Yellow' removed? {isColorRemoved}");
+
+            PrintColors(colors);
+
+            colors.Add("Black");
+            colors.Add("Yellow");
+            colors.Add("Green");
+            colors.Add("Black");
+
+            // Remove all occurrences of "Black" from the list
+            colors.RemoveAll(color => color == "Black");
+
+            PrintColors(colors);
+        } 
     }
 }
