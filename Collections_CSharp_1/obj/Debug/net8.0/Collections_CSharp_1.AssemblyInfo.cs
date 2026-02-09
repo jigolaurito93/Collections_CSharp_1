38d971c4ed363b0f640e2dafdaa142c917ac48df
jigolaurito93 <jigolaurito93@yahoo.com>
2026-02-09 05:45:36 -0600

    builld new assemblyinfo

--- Collections_CSharp_1/obj/Debug/net8.0/Collections_CSharp_1.AssemblyInfo.cs
+++ Collections_CSharp_1/obj/Debug/net8.0/Collections_CSharp_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collections_CSharp_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8bedc4170e422b6906ba16552ae47376044b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80814f95b0dfc87d3534ce3705d038650974d8ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collections_CSharp_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collections_CSharp_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
