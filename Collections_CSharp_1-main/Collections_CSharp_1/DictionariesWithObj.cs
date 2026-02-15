ebbf0614c4ab0a78edc1bd9abd720d58c68decb3
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-14 23:09:26 -0500

    using strings as keys in dictionary

--- Collections_CSharp_1-main/Collections_CSharp_1/DictionariesWithObj.cs
+++ Collections_CSharp_1-main/Collections_CSharp_1/DictionariesWithObj.cs
@@ -6,6 +6,7 @@ namespace Collections_CSharp_1
     {
         public static void UsingDictionariesWithObj()
         {
+            /*
             Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
             
             employees.Add(101, new Employee("Peter Jackson", 42, 80000));
@@ -16,6 +17,41 @@ namespace Collections_CSharp_1
             {
                 Console.WriteLine($"ID: {item.Key} named: {item.Value.Name} earns {item.Value.Salary} and is {item.Value.Age} years old.");
             }
+            */
+
+            // Using strings as Key
+            // When using strings as key, you have to put square brackets around it.
+            var localStates = new Dictionary<string, string>
+            {
+                ["NY"] = "New York",
+                ["CA"] = "California",
+                ["IL"] = "Illinois"
+            };
+
+            // Using TryGetValue to check if a Value is in a dictionary by using its Key as an argument, 
+            // then use the keyword "out", then declare the data type, then the variable name. It returns a boolean
+            if (localStates.TryGetValue("IN", out string state))
+            {
+                Console.WriteLine(state);
+            }
+            else
+            {
+                Console.WriteLine("Not in dictionary");
+            };
+
+            // Using foreach loop to iterate every item in the dictionary
+            foreach (var (code, statue) in localStates)
+            {
+                Console.WriteLine($"The state code is {code} for the state of {statue}.");
+            };
+
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(20);
+            list.AddLast(10);
+            list.AddFirst(5);
+
+            foreach (var item in list)
+            Console.WriteLine(item);
         }
     }
 }
